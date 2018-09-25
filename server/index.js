2c2fa0f4886e8816d7c92c28c0c69e1c3a20b490
nick567187 <nchang.edu@gmail.com>
2018-09-25 16:42:36 -0700

    update server

--- server/index.js
+++ server/index.js
@@ -43,6 +43,37 @@ app.get('/:idOrName/users', cors(corsOptions), (req, res) => {
   });
 });
 
+app.get('/api/photos/:idOrName/restaurants', cors(corsOptions), (req, res) => {
+  if(isNaN(parseInt(req.params.idOrName))) {
+    db.getAllPicturesByName(req.params.idOrName, (err,data) => {
+      if(err) {
+        res.send(err);
+      } else {
+        res.send(data);
+      }
+    });
+  } else {
+    db.getAllPicturesById(req.params.idOrName, (err, data) => {
+      if(err) {
+        res.send(err);
+      } else {
+        res.send(data);     
+      }
+    });   
+  }
+});
+
+app.get('/api/photos/:idOrName/users', cors(corsOptions), (req, res) => {
+  db.getAllUsers(req.query.users, (err, data) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(data);     
+    }
+  });
+});
+
+
 var port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log("Connected on port 3001"));
